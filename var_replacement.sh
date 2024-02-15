2965df65692706bd212647e0cccacbb6e6cab6eb
juanfdzz <jfernandez@stemdo.io>
2024-02-15 11:56:14 +0100

    crazytest

--- var_replacement.sh
+++ var_replacement.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
+
 set -x
-# Obtén los nombres de los archivos y los secretos de las variables de entorno
-filenames=("${filename}")
-secrets=("${secrets}")
-secrets_name=("${secrets_names}")
-echo $secrets_name
-echo $secrets
-echo $filenames
-# Recorre cada archivo
-for file in "${filenames[@]}"; do
-  # Busca el archivo en todos los directorios
-  find / -type f -name $file 2>/dev/null | while read -r filepath; do
-    echo "Procesando el archivo $filepath"
-    # Recorre cada nombre de secreto
-    for index in "${!secret_names[@]}"; do
-      # Obtiene el nombre y el valor del secreto
-      secret_name=${secret_names[index]}
-      secret_value=${secrets[index]}
-      # Reemplaza todas las ocurrencias del nombre del secreto en el archivo, sin tener en cuenta mayúsculas o minúsculas
-      sed -i "s/\$\{\{ $secret_name \}\}/$secret_value/Ig" $filepath
-    done
-    # Muestra el contenido del archivo
-    cat "$filepath"
+
+# El nombre del archivo con la lista de nombres de secretos separados por espacios
+SECRETS_NAMES=$1
+
+# Los nombres de los archivos donde se realizará la sustitución
+FILES_TO_UPDATE=$2
+
+# Recuperamos los nombres de los secretos y los almacenamos en un array
+IFS=' ' read -ra SECRET_NAMES <<< "$SECRETS_NAMES"
+
+# Función para reemplazar las variables de entorno en un archivo
+replace_secrets() {
+  local file="$1"
+  for i in "${!SECRET_NAMES[@]}"; do
+    # Obtenemos el valor del secreto desde las variables de entorno
+    secret_value="${!SECRET_NAMES[$i]}"
+
+    # Preparamos la expresión regular para buscar la variable de entorno en el formato ${{ variable }}
+    regex="\${{ ${SECRET_NAMES[$i]} }}}"
+
+    # Usamos sed para reemplazar las variables en el archivo
+    sed -i "s|$regex|$secret_value|g" "$file"
   done
+}
+
+# Dividimos los nombres de los archivos en un array
+IFS=' ' read -ra FILES_ARRAY <<< "$FILES_TO_UPDATE"
+
+# Buscamos los archivos por nombre y aplicamos la función de reemplazo
+for file_pattern in "${FILES_ARRAY[@]}"; do
+  find . -type f -name "$file_pattern" -exec bash -c 'replace_secrets "{}"' \;
 done
 
 cat apps/pro/k8s/configmap.yaml
