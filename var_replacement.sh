42a055c056713f6c7f21041481be4df21d8e0115
juanfdzz <jfernandez@stemdo.io>
2024-02-15 10:51:32 +0100

    a

--- var_replacement.sh
+++ var_replacement.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-cat $0
 set -x
 # Obtén los nombres de los archivos y los secretos de las variables de entorno
 filenames=("${filename}")
 secrets=("${secrets}")
+secrets_name=("${secrets_names}")
 
 # Recorre cada archivo
 for file in "${filenames[@]}"; do
   # Busca el archivo en todos los directorios
   find / -type f -name $file 2>/dev/null | while read -r filepath; do
     echo "Procesando el archivo $filepath"
-    # Recorre cada secreto
-    for secret in "${secrets[@]}"; do
+    # Recorre cada nombre de secreto
+    for index in "${!secret_names[@]}"; do
+      # Obtiene el nombre y el valor del secreto
+      secret_name=${secret_names[index]}
+      secret_value=${secrets[index]}
       # Reemplaza todas las ocurrencias del nombre del secreto en el archivo, sin tener en cuenta mayúsculas o minúsculas
-      sed -i "s/\$\{\{ $secret \}\}/$(eval echo \$$secret)/Ig" $filepath
+      sed -i "s/\$\{\{ $secret_name \}\}/$secret_value/Ig" $filepath
     done
     # Muestra el contenido del archivo
     cat "$filepath"
   done
 done
-
-echo echo dolar secrets debajo?
-echo "$secrets"
 
 cat apps/pro/k8s/configmap.yaml
