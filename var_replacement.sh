24ed059a172a5685954497ce54052ed38729bc8a
juanfdzz <jfernandez@stemdo.io>
2024-02-15 09:39:02 +0100

    testing

--- var_replacement.sh
+++ var_replacement.sh
@@ -6,15 +6,15 @@ secrets=("${secrets}")
 
 # Recorre cada archivo
 for file in "${filenames[@]}"; do
-  # Asegúrate de que el archivo existe antes de intentar modificarlo
-  if [[ -f $file ]]; then
-    echo "Procesando el archivo $file"
+  # Busca el archivo en todos los directorios
+  find / -type f -name $file 2>/dev/null | while read -r filepath; do
+    echo "Procesando el archivo $filepath"
     # Recorre cada secreto
     for secret in "${secrets[@]}"; do
       # Reemplaza todas las ocurrencias del nombre del secreto en el archivo
-      sed -i "s/$secret/$(eval echo \$$secret)/g" $file
+      sed -i "s/$secret/$(eval echo \$$secret)/g" $filepath
     done
-  else
-    echo "El archivo $file no existe"
-  fi
+  done
+  cat "$filepath"
 done
+
