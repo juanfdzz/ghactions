f76ef7fc174f81165d873f7b84a46cd0529623c7
juanfdzz <jfernandez@stemdo.io>
2024-02-15 13:22:18 +0100

    a

--- var_replacement.sh
+++ var_replacement.sh
@@ -9,12 +9,13 @@ IFS=' ' read -ra SECRETS_ARRAY <<< "${secrets_replacement}"
 for i in "${SECRETS_ARRAY[@]}"; do
   # Obtenemos el valor del secreto desde las variables de entorno
   secret_value="${SECRETS_ARRAY[$i]}"
-
+  echo $secret_value
   # Creamos una variable de entorno con el nombre del secreto y su valor
   #export "${SECRET_NAMES_ARRAY[$i]}=$secret_value"
 
   # Preparamos la expresión regular para buscar la variable de entorno en el formato ${{ variable }}
   regex="\${{ ${SECRET_NAMES[$i]} }}"
+  echo $regex
 
   # Usamos sed para reemplazar las variables en los archivos
   for file in $filenames; do
