f98d404afdb00fab72aac92c30a1f44d417f57eb
juanfdzz <jfernandez@stemdo.io>
2024-02-15 11:58:20 +0100

    a

--- var_replacement.sh
+++ var_replacement.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-set -x
-
-# El nombre del archivo con la lista de nombres de secretos separados por espacios
-SECRETS_NAMES=$1
-
-# Los nombres de los archivos donde se realizará la sustitución
-FILES_TO_UPDATE=$2
-
-# Recuperamos los nombres de los secretos y los almacenamos en un array
-IFS=' ' read -ra SECRET_NAMES <<< "$SECRETS_NAMES"
-
-# Función para reemplazar las variables de entorno en un archivo
+# Define the function before calling it
 replace_secrets() {
   local file="$1"
   for i in "${!SECRET_NAMES[@]}"; do
-    # Obtenemos el valor del secreto desde las variables de entorno
+    # Obtain the secret value from environment variables
     secret_value="${!SECRET_NAMES[$i]}"
 
-    # Preparamos la expresión regular para buscar la variable de entorno en el formato ${{ variable }}
+    # Prepare the regular expression to search for the environment variable in the format ${{ variable }}
     regex="\${{ ${SECRET_NAMES[$i]} }}}"
 
-    # Usamos sed para reemplazar las variables en el archivo
+    # Use sed to replace the variables in the file
     sed -i "s|$regex|$secret_value|g" "$file"
   done
 }
+
+# The list of secret names separated by spaces
+SECRETS_NAMES=$1
+
+# Recover the secret names and store them in an array
+IFS=' ' read -ra SECRET_NAMES <<< "$SECRETS_NAMES"
+
+# Search for files by name and apply the replacement function
+for file_pattern in "$@"; do
+  find . -type f -name "$file_pattern" -exec bash -c 'replace_secrets "{}"' \;
+done
 
 # Dividimos los nombres de los archivos en un array
 IFS=' ' read -ra FILES_ARRAY <<< "$FILES_TO_UPDATE"
