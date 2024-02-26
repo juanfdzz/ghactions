55d6ce8eb67ca007c63878f2598b88b963b205f3
juanfdzz <juan.fdez22@gmail.com>
2024-02-26 16:51:41 +0100

    t

--- secrets_replc.sh
+++ secrets_replc.sh
@@ -2,22 +2,22 @@
 
 file="${filenames}"
 echo "${secrets}" #| jq -r 'to_entries | .[] | .key + "=" + .value' > secrets.txt
-
+echo ${secrets}
 
 # Reemplaza los valores en cada archivo
-while IFS= read -r line; do
-    key=$(echo "$line" | cut -d'=' -f1)
-    value=$(echo "$line" | cut -d'=' -f2)
-    echo $key
-    echo $value
-    if [[ ! "$key" == *"PRIVATE_KEY"* ]]; then
-        sed -i "s/__${key}__/${value}/g" "$file"
-    else
-        echo "$value" | awk '{print "                          " $0}' > key.pem
-        sed -i "/__${key}__/r key.pem" $file
-        sed -i "/__${key}__/d" $file
-    fi
-done < "secrets.txt"
+# while IFS= read -r line; do
+#     key=$(echo "$line" | cut -d'=' -f1)
+#     value=$(echo "$line" | cut -d'=' -f2)
+#     echo $key
+#     echo $value
+#     if [[ ! "$key" == *"PRIVATE_KEY"* ]]; then
+#         sed -i "s/__${key}__/${value}/g" "$file"
+#     else
+#         echo "$value" | awk '{print "                          " $0}' > key.pem
+#         sed -i "/__${key}__/r key.pem" $file
+#         sed -i "/__${key}__/d" $file
+#     fi
+# done < "secrets.txt"
 
 
-cat apps/pro/k8s/asd.yaml
+# cat apps/pro/k8s/asd.yaml
