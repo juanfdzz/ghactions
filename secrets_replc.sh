872af926a168150d0a63e0f17270f6e190b04317
juanfdzz <juan.fdez22@gmail.com>
2024-02-26 11:59:14 +0100

    t

---
+++ secrets_replc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Obtén los nombres de los archivos y los secretos desde las variables de entorno
+files="${filenames}"
+secrets="${secrets}"
+
+# Convierte el JSON de los secretos en un array asociativo en Bash
+declare -A secret_array
+eval "declare -A secret_array=$(echo "$secrets" | jq -r 'to_entries | .[] | .key + "=" + (.value|tostring)' | sed 's/\"//g')"
+
+# Itera sobre cada archivo especificado
+for file in $files; do
+    # Itera sobre cada secreto
+    for secret_key in "${!secret_array[@]}"; do
+        # Reemplaza la clave del secreto con su valor en el archivo
+        sed -i "s/__${secret_key}__/${secret_array[$secret_key]}/g" "$file"
+    done
+done
+cat asd.yaml
+cat asding.yaml
--- var_replacement.sh
+++ var_replacement.sh
@@ -1,21 +1,20 @@
-#!/bin/bash
+!/bin/bash
 
 
-# IFS=' ' read -ra SECRET_NAMES_ARRAY <<< "${secrets_names}"
-# IFS=' ' read -ra FILES_ARRAY <<< "${filenames}"
-# IFS=' ' read -ra SECRETS_ARRAY <<< "${secrets_replacement}"
+IFS=' ' read -ra SECRET_NAMES_ARRAY <<< "${secrets_names}"
+IFS=' ' read -ra FILES_ARRAY <<< "${filenames}"
+IFS=' ' read -ra SECRETS_ARRAY <<< "${secrets_replacement}"
 
-# length=${#SECRETS_ARRAY[@]}
-# for ((i=0; i<$length; i++)); do
+length=${#SECRETS_ARRAY[@]}
+for ((i=0; i<$length; i++)); do
 
-#   secret_value="${SECRETS_ARRAY[$i]}"
-#   echo $secret_value
+  secret_value="${SECRETS_ARRAY[$i]}"
+  echo $secret_value
 
-#   regex="\${{ ${SECRET_NAMES_ARRAY[$i]} }}"
-#   echo $regex
+  regex="\${{ ${SECRET_NAMES_ARRAY[$i]} }}"
+  echo $regex
 
-#   for file in $filenames; do
-#     find . -type f -name "$file" | while read -r filepath; do sed -i "s|$regex|$secret_value|Ig" "$filepath"; done
-#   done
-# done
-echo ${secrets}
+  for file in $filenames; do
+    find . -type f -name "$file" | while read -r filepath; do sed -i "s|$regex|$secret_value|Ig" "$filepath"; done
+  done
+done
