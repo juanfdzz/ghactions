7350b78bb861cb5a521c02d6267b12461c5f9110
juanfdzz <juan.fdez22@gmail.com>
2024-02-26 16:12:15 +0100

    test

--- secrets_replc.sh
+++ secrets_replc.sh
@@ -13,7 +13,7 @@ while IFS= read -r line; do
     if [[ ! "$key" == *"PRIVATE_KEY"* ]]; then
         sed -i "s/__${key}__/${value}/g" "$file"
     else
-        echo "${{ $value }}" | awk '{print "                          " $0}' > key.pem
+        echo "$value" | awk '{print "                          " $0}' > key.pem
         sed -i "/__${key}__/r key.pem" $file
         sed -i "/__${key}__/d" $file
     fi
