a815e8e978a398054596f0b564411ef46df81fdd
juanfdzz <juan.fdez22@gmail.com>
2024-02-27 09:51:20 +0100

    t

--- secrets_replc.sh
+++ secrets_replc.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-set -x
 file="${filenames}"
-echo "${secrets}" | jq -r 'to_entries | .[] | .key + "=" + .value' > secrets.txt
-cat secrets.txt
+keys=$(echo "${secrets}" | | jq -r 'keys[]')
+values=$(echo "${secrets}" | | jq -r '.[]')
 
-while IFS= read -r line; do
-    key=$(echo "$line" | cut -d'=' -f1)
-    value=$(echo "$line" | cut -d'=' -f2)
-    echo $key
-    echo $value
-    if [[ ! "$key" == *"PRIVATE_KEY"* ]]; then
-        sed -i "s/__${key}__/${value}/g" "$file"
-    else
-        echo "$value" | awk '{print "                          " $0}' > key.pem
-        sed -i "/__${key}__/r key.pem" $file
-        sed -i "/__${key}__/d" $file
-    fi
-done < "secrets.txt"
+cat $keys
+echo valores
+cat $values
+# while IFS= read -r line; do
+#     key=$(echo "$line" | cut -d'=' -f1)
+#     value=$(echo "$line" | cut -d'=' -f2)
+#     echo $key
+#     echo $value
+#     if [[ ! "$key" == *"PRIVATE_KEY"* ]]; then
+#         sed -i "s/__${key}__/${value}/g" "$file"
+#     else
+#         echo "$value" | awk '{print "                          " $0}' > key.pem
+#         sed -i "/__${key}__/r key.pem" $file
+#         sed -i "/__${key}__/d" $file
+#     fi
+# done < "secrets.txt"
 
 
