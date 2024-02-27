ae03607b3eb183b8b77e44d099e02c8f32b1f381
juanfdzz <juan.fdez22@gmail.com>
2024-02-27 10:12:29 +0100

    test

--- secrets_replc.sh
+++ secrets_replc.sh
@@ -1,22 +1,46 @@
 #!/bin/bash
 file="${filenames}"
+secret_file="${secrets}"
+# Extraer las claves y valores de la cadena JSON
 # keys=$(echo "${secrets}" | jq -r 'keys[]')
 # values=$(echo "${secrets}" | jq -r '.[]')
 
-echo "${secrets}"
-echo "Probando desde un cat a un fichero"
-echo "${secrets}" > secrets.txt
-cat secrets.txt
-# Extraer las claves y valores de la cadena JSON
-keys=$(echo "${secrets}" | jq -r 'keys[]')
-values=$(echo "${secrets}" | jq -r '.[]')
 
-# Imprimir las claves y valores
-for key in $keys; do
-    value=$(echo "${secrets}" | jq -r ".$key")
-    echo "Clave: $key"
-    echo "Valor: $value"
-done
+# Leer el archivo de secretos línea por línea
+while IFS= read -r line; do
+    # Extraer la clave y el valor de la línea
+    key=$(echo "$line" | jq -r 'keys[]')
+    value=$(echo "$line" | jq -r 'values[]')
+
+    # Reemplazar cada ocurrencia de la clave en el archivo con su valor correspondiente
+    if [[ ! "$key" == *"PRIVATE_KEY"* ]]; then
+        sed -i "s/__${key}__/${value}/g" "$file"
+    else
+        # Guardar el valor en un archivo temporal respetando los saltos de línea
+        echo "$value" > key_temp.pem
+        # Insertar el contenido del archivo temporal en el lugar correspondiente en el archivo
+        sed -i "/__${key}__/r key_temp.pem" "$file"
+        # Eliminar la línea con la clave del archivo
+        sed -i "/__${key}__/d" "$file"
+        # Eliminar el archivo temporal
+        rm key_temp.pem
+    fi
+done < "$secret_file"
+
+
+cat $file
+
+
+
+
+# abajo justo funciona
+# for key in $keys; do
+#     value=$(echo "${secrets}" | jq -r ".$key")
+#     echo "Clave: $key"
+#     echo "Valor: $value"
+# done
+
+
 # while IFS= read -r line; do
 #     key=$(echo "$line" | cut -d'=' -f1)
 #     value=$(echo "$line" | cut -d'=' -f2)
